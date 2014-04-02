7cda515f6dde6afc5be28c662a938c0c76596fcf
mr.Shu <mr@shu.io>
2014-04-02 23:26:43 +0200

    get_text action

--- js/scraper.js
+++ js/scraper.js
@@ -1,1 +1,6 @@
-console.log($('body').text());
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+            if (request.action == "get_text")
+                sendResponse({text: $('div').text()});
+    });
