70cd21cd7040bc56b538fc32550b5b93594e79b1
mr.Shu <mr@shu.io>
2014-04-03 23:56:55 +0200

    generalized

--- js/background.js
+++ js/background.js
@@ -4,9 +4,13 @@ function Background()
 
 Background.prototype.get_text = function() {
     chrome.tabs.query({}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "get_text"}, function(response) {
-            console.log(response.text);
-        });
+        tabs.forEach(function(e, i ,a) {
+            console.log(tabs[i]);
+            if (tabs[i].url.indexOf("chrome") == 0) return;
+            chrome.tabs.sendMessage(tabs[i].id, {action: "get_text"}, function(response) {
+                console.log(response.text);
+            });
+        })
     });
 }
 
