c13e82227514e1afeff4ab24cf13733d29f450bc
mr.Shu <mr@shu.io>
2014-04-06 19:28:57 +0200

    cleanup and callback

--- js/background.js
+++ js/background.js
@@ -5,14 +5,22 @@ function Background()
 Background.prototype.get_text = function(callback) {
     var out = new Array();
     chrome.tabs.query({}, function(tabs) {
-        tabs.forEach(function(e, i ,a) {
+        var t = new Array();
+
+        tabs.forEach(function(e, i, a){
             // ignoring internal urls
             if (tabs[i].url.indexOf("chrome") == 0) return;
+            t.push(e);
+        });
+
+        t.forEach(function(e, i ,a) {
             chrome.tabs.sendMessage(tabs[i].id, {action: "get_text"}, function(response) {
                 out.push({text: response.text, url: tabs[i].url, title: response.title});
+
+                if (t.length == i+1)
+                    return callback(out);
             });
         });
-        callback(out);
     });
 }
 
